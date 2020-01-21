08a15ac52291f03b9cd32f2e93291fa051ee814f
Iñigo <inigo.gorosabel@gmail.com>
2020-01-21 13:02:48 +0100

    Esqueleto de las primeras funciones

---
+++ app/controller/api.php
@@ -0,0 +1,11 @@
+<?php
+class api extends OController{
+  /*
+   * Función para registrar un nuevo jugador
+   */
+  function register($req){}
+  /*
+   * Función para iniciar sesión
+   */
+  function login($req){}
+}
---
+++ app/controller/home.php
@@ -0,0 +1,15 @@
+<?php
+class home extends OController{
+  /*
+   * Página temporal, sitio cerrado
+   */
+  function closed($req){}
+  /*
+   * Home pública
+   */
+  function index($req){}
+  /*
+   * Página de error 404
+   */
+  function notFound($req){}
+}
